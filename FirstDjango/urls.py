beb765a25511a474fc63b31b2c9f8e0bcdc4ef94
ritztoston <ritztoston@gmail.com>
2018-10-04 19:46:28 +0800

    With html, static images

--- FirstDjango/urls.py
+++ FirstDjango/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
 from first_app import views
+
 
 urlpatterns = [
     path('', views.index, name='index'),
--- first_app/views.py
+++ first_app/views.py
@@ -3,4 +3,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello Word!");
+    my_dictionary = {'insert_me':"Hello"}
+    return render(request, 'first_app/index.html', context=my_dictionary)
